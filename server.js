0729c46f72fb409e3061325747134b771cce8f16
Lakeram Narine <nar19002@byui.edu>
2022-05-10 21:36:57 -0600

    Added swagger doc

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongodb = require('./db/connect');
 const routes = require('./routes/contacts');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 //Middlewares
+var options = {
+  explorer: true
+};
+
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.json());
 
